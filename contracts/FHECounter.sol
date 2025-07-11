f0a9dc18b7caa15cb1dbbc8e6fed8ece3638727f
freedom0010 <162872021+freedom0010@users.noreply.github.com>
2025-07-11 21:51:30 +0800

    Add files via upload

--- contracts/FHECounter.sol
+++ contracts/FHECounter.sol
@@ -1,1 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
 
+import {FHE, euint32} from "@fhenixprotocol/contracts/FHE.sol";
+import "@fhenixprotocol/contracts/BaseFHE.sol";
+
+contract FHECounter is BaseFHE {
+    euint32 private counter;
+
+    constructor() {
+        counter = FHE.asEuint32(0);
+    }
+
+    function increment() public {
+        counter = counter + FHE.asEuint32(1);
+    }
+
+    function getCounter() public view returns (euint32) {
+        return counter;
+    }
+}
