fc9499879e3c629fbbb12549ea3a6ad05e5be060
freedom0010 <162872021+freedom0010@users.noreply.github.com>
2025-07-11 21:52:02 +0800

    Add files via upload

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,1 +1,14 @@
+const hre = require("hardhat");
 
+async function main() {
+  const FHECounter = await hre.ethers.getContractFactory("FHECounter");
+  const counter = await FHECounter.deploy();
+  await counter.deployed();
+
+  console.log("FHECounter deployed to:", counter.address);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
